a2a73d9aeb64393c153f3d8ce4d6ac985409cecd
Rene Fuentes <ralex.4687backup@gmail.com>
2026-03-13 23:06:06 -0300

    agregue nuevas fotos con otro formato y agregue imagen a menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 const productos = [
-    { id: 1, nombre:"Pizza Muzzarella", precio: 11500 }, 
+    { id: 1, nombre:"Pizza Muzzarella", precio: 11500, imagen:"img/pizza-muzarella.webp" },
 
-    { id: 2, nombre:"Pizza Napolitana", precio: 12500, },
-    { id: 3,nombre:"Pizza Cuatro quesos", precio: 14500 },
-    { id: 4, nombre:"Pizza Jamon y morron", precio: 13500 },
-    { id: 5, nombre:"Empanada de Jamon y queso", precio: 2500 },
-    { id: 6, nombre:"Empanada de Capresse", precio: 2500 },
-    { id: 7, nombre:"Empanada de Calabaza y queso", precio: 2500 },
-    { id: 8, nombre:"Empanada Panceta y ciruela", precio: 2500 },
+    { id: 2, nombre:"Pizza Napolitana", precio: 12500, imagen:"img/pizza-napo.webp" },
+    { id: 3,nombre:"Pizza Cuatro quesos", precio: 14500, imagen:"img/pizza-cuatroqueso.webp" },
+    { id: 4, nombre:"Pizza Jamon y morron", precio: 13500, imagen:"img/pizza-jamonymorron.webp" },
+    { id: 5, nombre:"Empanada de Jamon y queso", precio: 2500, imagen:"img/empanada-jyq.webp" },
+    { id: 6, nombre:"Empanada de Capresse", precio: 2500, imagen:"img/empanada-caprese.webp" },
+    { id: 7, nombre:"Empanada de Calabaza y queso", precio: 2500, imagen:"img/empanada-calabaza.webp" },
+    { id: 8, nombre:"Empanada Panceta y ciruela", precio: 2500, imagen:"img/empanada-panceta.webp" },
 ];
 
 let cartProducts = JSON.parse(localStorage.getItem("cartProducts")) || [];
@@ -19,15 +19,22 @@ function renderProductos(productsArray) {
 
     productsArray.forEach(producto => {
         const card = document.createElement("div")
-        card.innerHTML = `<h3>${producto.nombre}</h3>
-                          <h4>$${producto.precio}</h4>
-                          <div class="cantidad-container">
-                            <button class="restar">-</button>
-                            <span class="cantidad">1</span>
-                            <button class="sumar">+</button>
+        
+        card.innerHTML = `
+             <div class="img-wrapper">
+                  <img src="${producto.imagen}" alt="${producto.nombre}">
+             </div>
+             <div class="card-body">
+                            <h3>${producto.nombre}</h3>
+                            <h4>$${producto.precio}</h4><div class="cantidad-container">
+                            <div class="cantidad-container">
+                                <button class="restar">-</button>
+                                <span class="cantidad">1</span>
+                                <button class="sumar">+</button>
                           </div> 
                           <button class="productoAgregar" id="${producto.id}">Agregar al carrito</button>`;
-        productContainer.appendChild(card);
+        
+                          productContainer.appendChild(card);
 
 
         let cantidadLocal = 1;
@@ -78,6 +85,7 @@ function agregarAlCarrito () {
                     id: seleccionado.id,
                     nombre: seleccionado.nombre,
                     precio: seleccionado.precio,
+                    imagen: seleccionado.imagen,
                     cantidad: cantidadElegida
                 };
                   
